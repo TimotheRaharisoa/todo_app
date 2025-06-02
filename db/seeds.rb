617a61ebd80625495849553f8fa540960815cab3
Timothe Rop <timothe.raharisoa96@gmail.com>
2025-06-02 17:06:20 -0400

    FEAT: Add logic in seeds file to populate database with sample data

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,5 +3,29 @@
 #
 # Examples:
 #
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+
+
+puts "Seeding database..."
+
+# Create an admin user
+admin_user = User.create!(
+    username: "admin", 
+    first_name: "Admin",
+    last_name: "User",
+    email: "admin@example.com",
+    password: "password",
+    password_confirmation: "password"
+    )
+
+puts "Admin user created with email: #{admin_user.email} and password: #{admin_user.password}"
+# Create a sample task for the admin user   
+first_task = Task.create!(
+    title: "Sample Task",
+    description: "This is a sample task created during seeding.",
+    priority: "high",
+    due_date: Date.today + 7.days,
+    completed: false,
+    user: admin_user
+)
